11ce9706a9afadd56c33d750fa27ea799803eb51
Utashi Fujimoto <frusciante2580@gmail.com>
2026-02-10 23:50:02 +0000

    add: 雨の日も精進

--- ABC/ABC_417/A.cpp
+++ ABC/ABC_417/A.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 
 int main() {
+    int N, A, B;
+    std::cin >> N >> A >> B;
+
+    std::string S;
+    std::cin >> S;
+
+    for (int i = A; i < N - B; i++) {
+        std::cout << S[i];
+    }
+    std::cout << std::endl;
 
     return 0;
 }
--- typical90/s3/079.cpp
+++ typical90/s3/079.cpp
@@ -1,7 +1,52 @@
 #include <bits/stdc++.h>
 
-int main() {
+using ll = long long;
 
+int main() {
+    // input processing
+    int H, W;
+    std::cin >> H >> W;
+
+    std::vector<int> A(H * W);
+    for (int i = 0; i < H; i++) { // H for rownum
+        for (int j = 0; j < W; j++) { // W for colnum
+            std::cin >> A[j + i * W];
+        }
+    }
+    std::vector<int> B(H * W);
+    for (int i = 0; i < H; i++) { // H for rownum
+        for (int j = 0; j < W; j++) { // W for colnum
+            std::cin >> B[j + i * W];
+        }
+    }
+
+    // solve
+    ll ans = 0;
+    for (int i = 0; i < H * W; i++) {
+        int j = i + 1;
+        int k = i + W;
+        int l = i + W + 1;
+        ll d = B[i] - A[i];
+ 
+        if (i % W == W - 1) { // last column
+            if (d != 0) { std::cout << "No\n"; return 0; }
+            continue;
+        }
+        else if (i >= W * (H - 1)) { // last row
+            if (d != 0) { std::cout << "No\n"; return 0; }
+            continue;
+        }
+            else {
+            A[j] += d;
+            A[k] += d;
+            A[l] += d;
+            ans += std::llabs(d);
+        }
+    }
+
+    // presentation
+    std::cout << "Yes\n" << ans << std::endl;
+    
     return 0;
 }
 
