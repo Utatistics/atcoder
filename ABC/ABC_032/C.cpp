83f8791e4a49c3c2a8445c15071b30f397057b05
Utashi Fujimoto <frusciante2580@gmail.com>
2026-02-11 23:16:28 +0000

    add: 精進

--- ABC/ABC_032/C.cpp
+++ ABC/ABC_032/C.cpp
@@ -1,2 +1,48 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using ll = long long;
+
+int main () {
+    int N;
+    std::cin >> N;
+
+    ll K;
+    std::cin >> K;
+
+    std::vector<ll> S(N);
+    for (int i = 0; i < N; i++) std::cin >> S[i];
+
+    // edge cases
+    for (int i = 0; i < N; i++) {
+        if (S[i] == 0) {
+            std::cout << N << std::endl;
+            return 0;
+        }
+    }
+
+    if (K == 0) {
+        std::cout << 0 << std::endl;
+        return 0;
+    }
+
+    // shakutori
+    int ans = 0;
+
+    int r = 0;
+    ll s = 1;   // product starts from 1
+    for (int l = 0; l < N; l++) {
+        while (r < N && s * S[r] <= K) {
+            s *= S[r];
+            r++;
+        }
+
+        ans = std::max(ans, r - l);
+
+        if (r == l) ++r;
+        else s /= S[l];
+    }
+
+    std::cout << ans << std::endl;
+    return 0;
+}
 
--- ABC/ABC_417/C.cpp
+++ ABC/ABC_417/C.cpp
@@ -1,16 +1,28 @@
 #include <bits/stdc++.h>
 
+using ll = long long;
+
 int main() {
-    int N, A, B;
-    std::cin >> N >> A >> B;
+    // input processing
+    int N;
+    std::cin >> N;
+    std::vector<int> A(N);
+    for (int i = 0; i < N; i++) std::cin >> A[i];
 
-    std::string S;
-    std::cin >> S;
+    // solve
+    int r = 0;
+    ll s = 0;
+    for (int l = 0; l < N; l++) {
+        while (r < N && /* condition */) {
+            // e.g. s += A[r];
+            r++;
+        }
 
-    for (int i = A - i < N - B; i++) {
-        std::cout << S[i];
+        // ans update here
+
+        if (r == l) ++r;
+        // e.g. else s -= A[l];
     }
-    std::cout << std::endl;
 
     return 0;
 }
