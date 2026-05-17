cba430495c2a0c5c8f3a60fb0a34f36546e0fc27
Utashi Fujimoto <frusciante2580@gmail.com>
2026-05-17 02:36:44 +0000

    add: 精進

--- ABC/ABC_369/C.cpp
+++ ABC/ABC_369/C.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 
-int main() {
+using ll = long long;
 
+int main() {
+    int N;
+    std::cin >> N;
+
+    std::vector<int> A(N);
+    for (int i = 0; i < N; i++) std::cin >> A[i];
+
+    if (N == 1) { // edgw case
+        std::cout << 1 << '\n';
+        return 0;
+    }
+
+    ll ans = 0;
+    int r = 1;
+    for (int l = 0; l < N; l++) {
+        int d = A[r] - A[r - 1];
+        while (r + 1 < N && d == A[r + 1] - A[r]) {
+            r++;
+        }
+        ans += (r - l) + 1;
+        if (r - l == 1 && r < N - 1) r++;
+    }
+
+    std::cout << ans << std::endl;
     return 0;
 }
 
--- ABC/ABC_429/D.cpp
+++ ABC/ABC_429/D.cpp
@@ -1,73 +1,41 @@
 #include <bits/stdc++.h>
 
 using ll = long long;
-
-int binary_search(int i, int key, const::std::vector<ll> &A) {
-    int left = i - 1; // i = 0 does not always satisfy condition
-    int right = (int)A.size();
-    while (right - left > 1) {
-        int mid = left + (right - left) / 2;
-        if (A[mid] - A[i + 1] >= key) right = mid; // argmin k (key < A[k]) *ascending
-        else left = mid;
-    }
-    return right; // (int)A.size() if not found. 
-}
+using P = std::pair<ll, int>;
 
 int main() {
-    // input processing
     int N, C;
     ll M;
     std::cin >> N >> M >> C;
 
-    std::map<ll, int> m;
+    std::map<ll, int> m; // {loc, cnt}
     for (int i = 0; i < N; i++) {
-        int a;
+        ll a;
         std::cin >> a;
-        m[a]++; m[a + M]++; // needs 2 times for val (not key)
+        m[a]++;
     }
-    int p;
-    int n = m.size(); // O(N)
+    int n = m.size();
 
-    p = 0;
-    std::vector<ll> K; // key (point of start)
-    for (auto [k, v] : m) {
-        if (p > n / 2 - 1) {
-            K.push_back(M);
-            break;
-        }
-        K.push_back(k);
-        p++;
-    }
-    
-    p = 1;
-    std::vector<ll> ps(n, 0);
-    for (auto [k, v] : m) { // value (cnt as per each loc)
-        ps[p] = ps[p - 1] + v;
-        p++;
-    }
-    
-    // solvne
+    std::vector<P> A;
+    for (auto [k, v] : m) A.push_back({k, v});
+    for (auto [k, v] : m) A.push_back({k + M, v});
+
     ll ans = 0;
-    for (int i = 0; K[i] < M; i++) {
-        int t = binary_search(i, C, ps); // get threshuold
-        ll c = K[i + 1] - K[i]; // number of duplicates
-        ans += (ps[t] - ps[i + 1]) * c;
+    ll pk = A[n - 1].first - M;
+    int r = 0, c = 0;
+    for (int l = 0; l < n; l++) {
+        while (c < C) {
+            c += A[r].second;
+            r++;
+        }
+        // l moving forward
+        auto [k, v] = A[l];
+        ans += 1LL * (k - pk) * c;
+        pk = k;
+        c -= v;
     }
-    
-    // presentation
+
     std::cout << ans << std::endl;
 
     return 0;
 }
-
-std::vector<ll> enum_divisors(ll N) 
-
-std::vector<ll> x;
-for (ll i = 1; i * i <= N; ++i) { // O(√N)
-    if (N % i == 0) {
-        x.push_back(i);
-        if (N / i != i) x.push_back(N / i); // avoid duplicates
-    }
-}
-std::sort(x.begin(), x.end()); // if necessaryB 
-
