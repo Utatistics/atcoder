b9695f76d7c5e0e2e0ee2552c0d881109333b1e1
Utashi Fujimoto <frusciante2580@gmail.com>
2026-07-09 23:07:31 +0000

    add: 精進

--- ABC/ABC_371/D.cpp
+++ ABC/ABC_371/D.cpp
@@ -1,7 +1,33 @@
 #include <bits/stdc++.h>
 
-int main() {
+using ll = long long;
 
+int main() {
+    int N;
+    std::cin >> N;
+
+    std::vector<int> X(N);
+    for (int i = 0; i < N; i++) std::cin >> X[i];
+    std::vector<int> P(N);
+    for (int i = 0; i < N; i++) std::cin >> P[i];
+    std::vector<ll> ps(N + 1, 0);
+    for (int i = 1; i <= N; i++) ps[i] = ps[i - 1] + P[i - 1];
+
+
+    int Q;
+    std::cin >> Q;
+
+    while (Q--) {
+        int L, R;
+        std::cin >> L >> R;
+        
+        int i = std::lower_bound(X.begin(), X.end(), L) - X.begin(); // i >= L 
+        int j = std::upper_bound(X.begin(), X.end(), R) - X.begin(); // j > R
+        
+        ll ans = ps[j] - ps[i];
+        std::cout << ans << "\n";
+    }
+    
     return 0;
 }
 
--- ABC/ABC_379/D.cpp
+++ ABC/ABC_379/D.cpp
@@ -1,6 +1,44 @@
 #include <bits/stdc++.h>
 
+using ll = long long;
+
 int main() {
+    int Q;
+    std::cin >> Q;
+
+    std::deque<ll> dq;
+
+    ll offset = 0;
+    while (Q--) {
+        int t;
+        std::cin >> t;
+     
+        if (t == 1) {
+            dq.push_front(offset); // store offset at birth
+        }
+        else if (t == 2) {
+            int T;
+            std::cin >> T;
+            offset += T;
+            
+        } 
+        else {
+            int H;
+            std::cin >> H;
+
+            int ans = 0;
+            while (!dq.empty()) {
+                ll x = dq.back();
+                if (offset - x >= H) {
+                    dq.pop_back();
+                    ++ans;
+                }
+                else break;
+            }
+            std::cout << ans << "\n";
+        }
+
+    }
 
     return 0;
 }
