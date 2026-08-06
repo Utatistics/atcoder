ffd75fc24f68d9c3d1d510789df21f09b87377e6
Utashi Fujimoto <frusciante2580@gmail.com>
2026-08-06 00:52:27 +0000

    add: 精進、連結成分=ufではない

--- ABC/ABC_350/C.cpp
+++ ABC/ABC_350/C.cpp
@@ -1,6 +1,34 @@
 #include <bits/stdc++.h>
 
+using P = std::pair<int, int>;
+
 int main() {
+    int N;
+    std::cin >> N;
+
+    std::vector<int> A(N), I(N + 1, -1);
+    for (int i = 0; i < N; i++) {
+        std::cin >> A[i];
+        I[A[i]] = i; // val 2 idx
+    }
+
+    int K = 0;
+    std::vector<P> ans;
+    for (int i = 0; i < N; i++) {
+        int j = I[i + 1];
+        if (i == j) continue; // no swap needed
+
+        std::swap(A[i], A[j]);
+        std::swap(I[A[i]], I[A[j]]); // update index after swap
+
+        ++K;
+        ans.emplace_back(i + 1, j + 1);
+    }
+
+    std::cout << K << std::endl;
+    for (auto [i, j] : ans)
+        std::cout << i << " " << j << "\n";
+    
 
     return 0;
 }
--- ABC/ABC_350/D.cpp
+++ ABC/ABC_350/D.cpp
@@ -1,7 +1,61 @@
 #include <bits/stdc++.h>
 
-int main() {
+using ll = long long;
 
+int main() {
+    int N, M;
+    std::cin >> N >> M;
+
+    std::vector<std::vector<int>> adj(N);
+    for (int i = 0; i < M; i++) {
+        int u, v;
+        std::cin >> u >> v;
+        u--; v--;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    std::vector<std::vector<int>> C; // connected componets
+    std::vector<int> m(N, -1); // to which componets
+    std::vector<int> cnts; // edge count for comp
+
+    auto bfs = [&](int s) -> void {
+        if (m[s] >= 0) return;
+
+        int i = (int)C.size();
+        cnts.push_back(0);
+
+        std::vector<int> c;
+        std::queue<int> q;
+        c.push_back(s);
+        q.push(s);
+        m[s] = i;
+
+        while (!q.empty()) {
+            int x = q.front(); q.pop();
+
+            for (auto neighbor : adj[x]) {
+                ++cnts[i];
+                if (m[neighbor] >= 0) continue;
+
+                c.push_back(neighbor);
+                m[neighbor] = i;
+                q.push(neighbor);
+            }
+        }
+
+        cnts[i] /= 2;
+        C.push_back(c);
+    };
+
+    for (int i = 0; i < N; i++) bfs(i);
+
+    ll ans = 0;
+    for (int i = 0; i < (int)C.size(); ++i) {
+        int s = C[i].size();
+        ans += 1LL * s * (s - 1) / 2 - cnts[i];
+    }
+
+    std::cout << ans << std::endl;
     return 0;
 }
-
