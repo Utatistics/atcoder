f9f2dcea39567013469f6e772fc55b1e87960c1e
Utashi Fujimoto <frusciante2580@gmail.com>
2026-08-06 00:51:13 +0000

    add: 精進、len+1/2

--- ABC/ABC_401/D.cpp
+++ ABC/ABC_401/D.cpp
@@ -9,8 +9,13 @@ int main() {
     std::string S;
     std::cin >> S;
 
-    int cnt = 0;
-    for (int i = 0; i < N; i++) { // '?' to '.' if allowed
+    auto f = [](int len) -> int { // returns max num of '?' to be filled with 'o'
+        if (len <= 0) return 0;
+        return (len + 1) / 2;
+    };
+
+    int cnt = 0; // counter for 'o'
+    for (int i = 0; i < N; i++) {
         if (S[i] == 'o') {
             ++cnt;
             continue;
@@ -19,51 +24,50 @@ int main() {
         bool l = (i > 0 && S[i - 1] == 'o');
         bool r = (i + 1 < N && S[i + 1] == 'o');
 
-        if (S[i] == '?' && (l || r))
+        if (S[i] == '?' && (l || r)) {
             S[i] = '.';
+        }
     }
+    int c = cnt; // constant cnt (i.e. lower bound for cnt)
 
     std::vector<P> W;
     std::vector<int> id(N, -1);
-
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) { // possibly addded 'o's given '?'s
         if (S[i] != '?') continue;
 
         int j = i;
-        while (j < N && S[j] == '?') {
+        while (j < N && S[j] == '?') { // window of consecutive '?'s
             id[j] = (int)W.size();
             ++j;
         }
 
         W.emplace_back(i, j);
-        cnt += (j - i + 1) / 2;
+        cnt += f(j - i); // constant count + bonus
         i = j - 1;
     }
 
     std::string ans = S;
-    if (K == 0) {
-        for (int i = 0; i < N; i++) {
-            if (ans[i] == '?')
-                ans[i] = '.';
-        }
-    } 
-    else {
-        for (int i = 0; i < N; i++) {
-            if (ans[i] != '?') continue;
+    for (int i = 0; i < N; i++) {
+        if (ans[i] != '?') continue;
 
-            auto [l, r] = W[id[i]];
+        auto [l, r] = W[id[i]]; // [l, r)
+        int base = cnt - f(r - l);
 
-            int x = cnt - (r - l + 1) / 2;
+        int maxd = base; // forcing '.' at i (e.g. ????.????)
+        maxd += f(i - l);
+        maxd += f(r - (i + 1));
 
-            // maximum o if position i is forced to '.'
-            x += (i - l + 1) / 2;
-            x += (r - i) / 2;
+        int maxo = base; // forching 'o' at i (e.g. ???.o.???)
+        maxo += 1;
+        maxo += f(i - l - 1);
+        maxo += f(r - (i + 1) - 1);
 
-            if (x >= K)
-                ans[i] = '?';
-            else
-                ans[i] = 'o';
-        }
+        bool flgd = c <= K && K <= maxd;
+        bool flgo = c + 1 <= K && K <= maxo;
+
+        if (flgd && flgo) ans[i] = '?';
+        else if (flgo) ans[i] = 'o';
+        else ans[i] = '.';
     }
 
     std::cout << ans << '\n';
