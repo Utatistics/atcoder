d5f5c10332470fb47d19702dbfe09bedfd7a3aea
Utashi Fujimoto <frusciante2580@gmail.com>
2026-08-06 00:49:39 +0000

    add: 精進、頂点倍化

--- ABC/ABC_410/D.cpp
+++ ABC/ABC_410/D.cpp
@@ -1,7 +1,47 @@
 #include <bits/stdc++.h>
 
-int main() {
+using P = std::pair<int, int>;
 
+int main() {
+    int N, M;
+    std::cin >> N >> M;
+
+    int W = 1<<10;
+
+    std::vector<std::vector<P>> adj(N);
+    for (int i = 0; i < M; i++) {
+        int u, v, w;
+        std::cin >> u >> v >> w;
+        u--; v--;
+        adj[u].emplace_back(v, w);
+    }
+
+    std::vector<bool> visited(N * W, false);
+    auto bfs = [&]() -> void {
+        std::queue<int> q;
+
+        q.push(0); // init
+        
+        while (!q.empty()) { 
+            int uw = q.front(); q.pop();
+            int u = uw / W, x = uw % W;
+            for (auto [v, w] : adj[u]) {
+                int vw = v*W + (x^w); // x^w = x XOR w
+                if (visited[vw]) continue;
+                visited[vw] = true;
+                q.push(vw);
+            }
+        }
+    };
+    bfs();
+
+    for (int i = 0; i < W; i++) {
+        if (visited[(N - 1) * W + i]) {
+            std::cout << i << std::endl;
+            return 0;
+        }
+    }
+    std::cout << -1 << std::endl;
     return 0;
 }
 
