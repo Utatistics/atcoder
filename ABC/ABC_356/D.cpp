543e19708c2c36f51e51311b727510e224f78dc0
Utashi Fujimoto <frusciante2580@gmail.com>
2026-08-09 01:43:23 +0000

    add: 精進、x >> i & 1, 1ll << i = 2^k, 2ll << i = 2^k+1

--- ABC/ABC_356/D.cpp
+++ ABC/ABC_356/D.cpp
@@ -1,7 +1,26 @@
 #include <bits/stdc++.h>
 
-int main() {
+using ll = long long;
+static constexpr int MOD = 998244353;
 
+int main() {
+    ll N, M;
+    std::cin >> N >> M;
+
+    ll ans = 0;
+    for (int i = 0; i < 60; i++) { // N < 2^60 (needs 2^0 - 2^59)
+        if (!(M >> i & 1)) continue; // see ith bit of M
+        ll p = 2ll << i; // cycle
+        ll r = N % p; // residual
+
+        ans += ((N - r) / 2);
+        if (r >= (1ll << i)) { // r >= 2^k -> additional 1s
+            ans += r - (1ll << i) + 1; // 
+        }
+        ans %= MOD;
+    }
+
+    std::cout << ans << std::endl;
     return 0;
 }
 
