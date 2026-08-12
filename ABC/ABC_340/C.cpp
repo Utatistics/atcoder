7ade2b41550e708eddc27d8bbb21006560326bd3
Utashi Fujimoto <frusciante2580@gmail.com>
2026-08-12 00:42:33 +0000

    add: 精進、2^60~=10^18、メモ化再帰

--- ABC/ABC_340/C.cpp
+++ ABC/ABC_340/C.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 
-int main() {
+using ll = long long;
 
+int main() {
+    ll N;
+    std::cin >> N;
+
+    std::map<ll, ll> m;
+    auto f = [&](auto f, ll n) -> ll {
+        if (n == 1) return 0LL;
+        if (m.count(n) > 0) return m[n];
+
+        ll a = n / 2, b = (n + 1) / 2;
+        m[n] = n + f(f, a) + f(f, b);
+        return m[n];
+
+    };
+
+    std::cout << f(f, N) << std::endl;
     return 0;
 }
 
